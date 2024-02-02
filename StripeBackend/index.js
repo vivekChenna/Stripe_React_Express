72842fcd857e2fdef5a8d895e6cf073ffc561b48
vivekChenna <chennavivek96@gmail.com>
2024-02-02 10:32:13 +0530

    completed basic practice

--- StripeBackend/index.js
+++ StripeBackend/index.js
@@ -1,13 +1,51 @@
 const express = require("express");
+const cors = require("cors");
+const stripe = require("stripe")(process.env.SECRET_KEY);
+const { v4: uuidv4 } = require("uuid");
+
 const app = express();
 require("dotenv").config();
 
-
+app.use(express.json());
+app.use(cors());
 
 const PORT = process.env.PORT;
 
 app.get("/", (req, res) => {
   res.send("Hello this is the home page");
+});
+
+app.post("/payments", (req, res) => {
+  const { token, product } = req.body;
+  console.log("product details");
+  console.log(product);
+  const idempotencyKey = uuidv4();
+
+  return stripe.customers
+    .create({
+      email: token.email,
+      source: token.id,
+    })
+    .then((customer) => {
+      stripe.charges.create(
+        {
+          amount: product.price * 100,
+          currency: "usd",
+          customer: customer.id,
+          receipt_email: token.email,
+          description: `Your Purchase of ${product.name}`,
+          shipping: {
+            name: token.card.name,
+            address: {
+              country: token.card.address_country,  
+            },
+          },
+        },
+        { idempotencyKey }
+      );
+    })
+    .then((result) => res.status(200).json(result))
+    .catch((err) => console.log(err));
 });
 
 app.listen(PORT, () => {
--- StripeFrontend/src/App.jsx
+++ StripeFrontend/src/App.jsx
@@ -1,35 +1,50 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import StripeCheckout from "react-stripe-checkout";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [product, setProduct] = useState({
+    name: "Stripe",
+    price: 10,
+    productBy: "FB",
+  });
+
+  const makePayment = async (token) => {
+    const body = {
+      token,
+      product,
+    };
+
+    const headers = {
+      "Content-Type": "application/json",
+    };
+
+    try {
+      const response = await fetch("http://localhost:8000/payments", {
+        method: "POST",
+        headers,
+        body: JSON.stringify(body),
+      });
+
+      console.log("i am called");
+      console.log(response);
+
+      const jsonData = await response.json();
+      console.log(jsonData);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="">
+      <StripeCheckout
+        stripeKey={import.meta.env.VITE_APP_PUBLISHED_KEY}
+        token={makePayment}
+        amount={product.price * 100}
+        name="Stripe Integration"
+      />
+    </div>
+  );
 }
 
-export default App
+export default App;
